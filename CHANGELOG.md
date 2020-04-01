74e85ae457639c96935f91cb406137f596733e3d
Boanerges <boanerges.nascimento@compasso.com.br>
2020-04-01 20:07:29 -0300

    ### Criação da extensão

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+
+All notable changes to the "update-occ-file" extension will be documented in this file.
+
+## [1.0.0]
+
+- Initial realise of Update OCC File Extension
---
+++ src/extension.enum.ts
@@ -0,0 +1,8 @@
+export enum EEnv {
+  devNode = '%DEV_NODE%',
+  devApiKey = '%DEV_API_KEY%',
+  prodNode = '%PROD_NODE%',
+  prodApiKey = '%PROD_API_KEY%',
+  uatNode = '%UAT_NODE%',
+  uatApiKey = '%UAT_API_KEY%'
+}
